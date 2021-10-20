8c271897cf50d17c21766e25bc01f077d36938f1
ManoelaAA <72052371+ManoelaAA@users.noreply.github.com>
2021-10-20 20:48:33 -0300

    programas e sites que serão abertos

---
+++ open.py
@@ -0,0 +1,89 @@
+import webbrowser as web
+import subprocess
+import time
+
+
+
+# ----- CONFIGURA O ENDEREÇO DA WEB ----- #
+path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
+
+
+
+#Programas que poderão ser abertos
+def programs(open_prog):
+    
+    none_prog = False
+
+    if "google" in open_prog:
+        
+        subprocess.Popen(['C:\Program Files\Google\Chrome\Application\chrome.exe '])
+        time.sleep(0.5)
+
+    if "vs code" in open_prog:
+
+        subprocess.Popen(r"C:\Users\manoe\AppData\Local\Programs\Microsoft VS Code\Code.exe")
+        time.sleep(0.5)
+
+    if "teams" in open_prog:
+
+        subprocess.Popen(r"C:\Users\manoe\AppData\Local\Microsoft\Teams\current\Teams.exe")
+        time.sleep(0.5)
+
+    if "powerpoint" in open_prog:
+        subprocess.Popen(r"C:\Program Files (x86)\Microsoft Office\root\Office16\POWERPNT.exe")
+        time.sleep(0.5)
+
+    if "word" in open_prog:
+
+        subprocess.Popen(r"C:\Program Files (x86)\Microsoft Office\root\Office16\WINWORD.exe")
+        time.sleep(0.5)
+    
+    if "discord" in open_prog:
+
+        subprocess.Popen(r"C:\Users\manoe\AppData\Local\Discord\app-1.0.9003\Discord.exe")
+        time.sleep(0.5)
+    
+    if "spotify" in open_prog:
+
+        subprocess.Popen(r"C:\Users\manoe\AppData\Roaming\Spotify\Spotify.exe")
+        time.sleep(0.5)
+
+    else:
+        none_prog = True
+    
+    return none_prog
+
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------- #
+
+
+
+#Sites que poderão ser abertos através do comando acesse
+def site(site_path):
+
+    none_site = False
+
+    if "youtube" in site_path:
+
+        web.get(path).open("https://www.youtube.com/")
+
+    elif "e-mail" in site_path:
+
+        web.get(path).open("https://mail.google.com/mail/u/0/?ogbl#inbox")
+
+    elif "drive" in site_path:
+
+        web.get(path).open("https://drive.google.com/drive/my-drive")
+
+    elif "netflix" in site_path:
+
+        web.get(path).open("https://www.netflix.com/browse")
+    
+    elif "disney" in site_path:
+
+        web.get(path).open("https://www.disneyplus.com/pt-br/home")
+
+    else:
+        none_site = True
+
+    return none_site
